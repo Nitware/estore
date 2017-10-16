112c636fde8ebf417559a3689058493c1dced126
nitware <daniel.egenti@nitware.com.ng>
2017-10-16 19:55:07 +0100

    After merging and running for the first time

---
+++ src/Libraries/SmartStore.Data/Migrations/201710161003515_promotionDataTest.cs
@@ -0,0 +1,16 @@
+namespace SmartStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class promotionDataTest : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
